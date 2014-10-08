674cf6eedcd1e35c6b3804ad91d6f3f9cc492f22
kemalersin <mail@kemalersin.com>
2014-10-08 17:53:53 +0300

    CCB Chrome uzantısının ilk versiyonu.

---
+++ README.md
@@ -0,0 +1,13 @@
+CCB
+===
+CoIntellect bakiyesinin BTC ve TL cinsinden deðerini gösteren Chrome uzantýsý.
+
+Nasýl Çalýþýr?
+--------------
+Euro bakiyesinin **DOGE** deðerini bulup, bu deðeri önce güncel **[Cryptsy.com](https://cryptsy.com)** **DOGE/BTC** market satýþ fiyatýyla **BTC**'ye çevirir, sonra **[BTCTurk.com](https://btcturk.com)** satýþ fiyatý üzerinden **TL** deðerini hesaplar.
+
+Nasýl Kullanýlýr?
+-----------------
+Bu uzantý sadece **Google Chrome** web tarayýcýsý ile çalýþmaktadýr. Uzantýyý Chrome'da kullanabilmek için menüden "**Araçlar/Uzantýlar**" kýsmýna girilip sayfanýn sað üst kýsmýndaki "**Geliþtirici modu**" seçeneði iþaretlendikten sonra "**Paketlenmemiþ uzantýyý yükle...**"  düðmesine týklanarak uzantý dosyalarýnýn bulunduðu klasör seçilmelidir.
+
+**Not:** DOGE ve BTC kurlarý CoIntellect ödeme sayfasý yüklendiðinde alýnmaktadýr. Anlýk deðiþimler için ödeme sayfasýný yenilemeniz gerekir.
---
+++ main.js
@@ -0,0 +1,61 @@
+﻿var bid = 0;
+var tradeVersion = Date.now();
+
+var cryptsy = new XMLHttpRequest;
+var btcTurk = new XMLHttpRequest;
+
+btcTurk.open('GET', 'https://www.btcturk.com/api/ticker?' + tradeVersion, false);
+cryptsy.open('GET', 'https://www.cryptsy.com/trades/ajaxlasttrades?' + tradeVersion, false);
+
+btcTurk.onreadystatechange = function () {
+    if (btcTurk.readyState == 4 && btcTurk.status == 200) {
+        var data = JSON.parse(btcTurk.responseText);
+        bid = data.Bid;
+    }
+}
+
+cryptsy.onreadystatechange = function () {
+    if (cryptsy.readyState == 4 && cryptsy.status == 200) {
+        var data = JSON.parse(cryptsy.responseText);
+
+        btcTurk.send();
+
+        document.addEventListener('keyup', function (event) {
+            var el = event.srcElement;
+
+            if (el.id == 'CollectForm_amount') {
+                if (!el.value) {
+                    var dcAmountInTL = document.getElementById('dcAmountInTL');
+
+                    if (dcAmountInTL) {
+                        dcAmountInTL.remove();
+                    }
+                }
+                else {
+                    var dcAmountInDogecoin = document.getElementById('dcAmountInDogecoin');
+                    var doge = parseFloat(dcAmountInDogecoin.innerText.replace('Doge', '').trim()) - 1;
+
+                    if (!isNaN(doge)) {
+                        var tl = 0;
+                        var sato = data['132'];
+                        var btc = (doge * sato).toFixed(8);
+
+                        btc -= btc / 100 * .25;
+                        btc = btc.toFixed(8);
+
+                        tl = ((btc - 0.001) * bid).toFixed(2);
+
+                        dcAmountInDogecoin.innerHTML +=
+                            '<br />' +
+                            '<span id="dcAmountInTL" style="color: #428bca;">' +
+                            btc + ' BTC<br />' +
+                            tl + ' TL' +
+                            '</span>';
+                    }
+                }
+            }
+        }, false);
+    }
+};
+
+cryptsy.send();
